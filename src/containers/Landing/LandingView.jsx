66c8079e7998416987b3b3cda435b93f169042c3
Ahmed Modan <ahmed.modan@gmail.com>
2016-03-14 23:24:22 -0700

    [feat] add basic styling and input fields to landing page

--- src/containers/Landing/LandingView.jsx
+++ src/containers/Landing/LandingView.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import { reduxForm } from 'redux-form';
 
-export default class LandingView extends React.Component {
+export class LandingView extends React.Component {
+  static propTypes = {
+    fields: PropTypes.object.isRequired,
+    handleSubmit: PropTypes.func.isRequired
+  }
+
   render() {
+    const { fields: { username, password }, handleSubmit } = this.props;
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div className="row">
+        <h1>Welcome to Modanterist</h1>
+        <p className="text-info" >Sign Up below to get started</p>
+        <div className="col-sm-3 col-md-3 col-md-offset-4 col-sm-offset-3">
+          <form className="form-horizontal" onSubmit={handleSubmit} >
+            <div className="form-group">
+              <label htmlFor="username-input" >Username</label>
+              <input type="text"
+                className="form-control"
+                placeholder="Username"
+                id="username-input"
+                { ...username }
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password-input">Password</label>
+              <input type="text"
+                className="form-control"
+                placeholder="Password"
+                id="password-input"
+                { ...password }
+              />
+            </div>
+            <div className="form-group">
+              <Link to="counter">
+                <button className="btn btn-default" type="submit">Sign Up</button>
+              </Link>
+            </div>
+          </form>
+          <p>
+            Already have an account?
+            <Link to="counter/hello"> Login here</Link>
+          </p>
+        </div>
       </div>
     );
   }
 }
+
+export default reduxForm({
+  form: 'login',
+  fields: ['username', 'password']
+})(LandingView);
