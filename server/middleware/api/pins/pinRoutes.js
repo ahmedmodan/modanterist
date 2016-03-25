d5b62d2ef8ff4ae2d32b8d27f798e2e8bc6326bc
Ahmed Modan <ahmed.modan@gmail.com>
2016-03-24 20:26:25 -0700

    [feat] create template for pin routes page

---
+++ server/middleware/api/pins/pinRoutes.js
@@ -0,0 +1,5 @@
+const pinRoutes = require('koa-router')();
+
+pinRoutes.post('/pins', function* () {
+
+});
--- server/middleware/api/routes.js
+++ server/middleware/api/routes.js
@@ -2,5 +2,6 @@ const router = require('koa-router')();
 const userRoutes = require('./users/userRoutes');
 
 router.use('/users', userRoutes.routes());
+router.user('/api/pins', pinRoutes.routes());
 
 module.exports = router;
