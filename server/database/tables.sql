22fcee52689d458050924f6f4adf78ee9b679389
Ahmed Modan <ahmed.modan@gmail.com>
2016-03-28 12:46:01 -0700

    [database] update tables

--- server/database/tables.sql
+++ server/database/tables.sql
@@ -2,26 +2,37 @@ DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS boards CASCADE;
 DROP TABLE IF EXISTS cards CASCADE;
 DROP TABLE IF EXISTS boards_cards CASCADE;
+
 CREATE TABLE users(
-  user_id serial PRIMARY KEY,
+  _id serial PRIMARY KEY,
   username varchar(160) UNIQUE,
   password varchar(160)
 );
 
 CREATE TABLE boards (
-  board_id serial PRIMARY KEY,
+  _id serial PRIMARY KEY,
   description varchar(50),
-  user_id int REFERENCES users(user_id)
+  user_id integer,
+  FOREIGN KEY (user_id) REFERENCES users(_id)
+);
+
+CREATE TABLE tags (
+  _id bigserial PRIMARY KEY,
+  tag varchar(30)
 );
 
 CREATE TABLE cards (
-  card_id bigserial PRIMARY KEY,
+  _id bigserial PRIMARY KEY,
   description varchar(250),
-  image_url varchar(250)
+  image_url varchar(250),
+  tags_id integer,
+  FOREIGN KEY (tags_id) REFERENCES tags(_id)
 );
 
 CREATE TABLE boards_cards (
-  card_id int REFERENCES cards (card_id) ON UPDATE CASCADE,
-  board_id int REFERENCES boards (board_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  card_id integer,
+  board_id integer,
+  FOREIGN KEY (card_id) REFERENCES cards (_id) ON UPDATE CASCADE,
+  FOREIGN KEY (board_id) REFERENCES boards (_id) ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT board_card_pk PRIMARY KEY (board_id, card_id) --composite id optimizes query when done using board_id
 );
