8d3ddf15739c813a93526c7be38e62cdb7bdc29b
Ahmed Modan <ahmed.modan@gmail.com>
2016-04-08 09:58:32 -0700

    [database] create db select query builder

--- server/database/dbHelpers.js
+++ server/database/dbHelpers.js
@@ -21,6 +21,9 @@ const tagInsertQueryBuilder = (tags) => {
   return `INSERT INTO tags VALUES ${formattedTags}`;
 };
 
+const tagsSelectQueryBuilder = (column, params) =>
+  `SELECT ${column} FROM tags WHERE tag = ANY('{${params.join(',')}}'::text[])`;
+
 const cardInsertQueryBuilder = (values) => {
   const tagIDs = values.tags.rows.reduce(
     (formattedTagIds, tagId) =>
@@ -34,6 +37,15 @@ const cardInsertQueryBuilder = (values) => {
 };
 
 
+const selectQueryBuilder = (table, data) => {
+  switch (table) {
+    case TAGS:
+      return tagsSelectQueryBuilder(data.column, data.params.split(' '));
+    default:
+    // TODO: CHANGE DEFAULT RETURN STATEMENT
+      return null;
+  }
+};
 
 const insertQueryBuilder = (table, values) => {
   switch (table) {
@@ -61,6 +73,7 @@ module.exports = {
   CARDS,
   TAGS,
   queryDB,
+  selectQueryBuilder,
   insertQueryBuilder,
   cloudinaryUpload
 };
