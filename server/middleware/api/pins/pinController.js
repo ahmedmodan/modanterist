f50ab3d9540154e77f5a2076428cc91041f80dae
Ahmed Modan <ahmed.modan@gmail.com>
2016-04-08 09:59:35 -0700

    [feat] save card to db

--- server/middleware/api/pins/pinController.js
+++ server/middleware/api/pins/pinController.js
@@ -1,12 +1,17 @@
 'use strict';
 
 const db = require('./../../../database/dbHelpers.js');
+const _id = '_id';
 
 const savePin = function* () {
   let imageData;
   const fields = this.request.body.fields;
   yield db.cloudinaryUpload(this.request.body.files.file.path, data => (imageData = data));
   fields.image_url = imageData.url;
+  yield db.queryDB(db.insertQueryBuilder(db.TAGS, fields.tags));
+  fields.tags = yield db.queryDB(
+    db.selectQueryBuilder(db.TAGS, { column: _id, params: fields.tags })
+  );
   const result = yield db.queryDB(db.insertQueryBuilder(db.CARDS, fields));
   if (result.command === 'INSERT' && result.rowCount === 1) {
     this.response.status = 200;
