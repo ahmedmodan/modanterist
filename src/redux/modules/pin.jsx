7984f6be959f1b5704dc0672590277ca09568c07
Ahmed Modan <ahmed.modan@gmail.com>
2016-04-13 12:29:38 -0700

    [feat] write actions for getPins and savePins

--- src/redux/modules/pin.jsx
+++ src/redux/modules/pin.jsx
@@ -1,11 +1,19 @@
 import pinState from './../states/pinState';
 
-export const POST_PIN = 'POST_PIN';
+
+export const CLOSE_MODAL = 'CLOSE_MODAL';
 export const FILE_PREVIEW = 'FILE_PREVIEW';
+export const SET_PIN_STATE = 'SET_PIN_STATE';
+
 
 export function logResponse(response) {
+  console.log('hey');
   console.log('this is the response', response);
 }
+
+// export function closeModal() {
+//   action:
+// }
 
 export function createFormData(payload) {
   const formData = new FormData();
@@ -24,8 +32,22 @@ export function savePin(payload) {
   return dispatch => fetch('/api/pins/savePin', {
     method: 'POST',
     body: formData
-  }).then(response => response.json())
-    .then(data => dispatch(logResponse(data)));
+  }).then(response => {
+    if (response.status === 200) {
+      dispatch(logResponse(response));
+    }
+  });
+}
+
+export function setPinState(payload) {
+  return { type: SET_PIN_STATE,
+           payload };
+}
+
+export function getPins(payload) {
+  return dispatch => fetch(`/api/pins/getPins?tags=${payload.join(',')}`)
+    .then(response => response.json())
+    .then(body => dispatch(setPinState(body)));
 }
 
 export function renderPreview(payload) {
@@ -40,10 +62,10 @@ export const actions = {
 
 export default function pinReducer(state = pinState, action) {
   switch (action.type) {
-    case POST_PIN:
-      return Object.assign({}, state, action.payload);
     case FILE_PREVIEW:
       return state.set('imagePreview', action.payload);
+    case SET_PIN_STATE:
+      return state.set('pins', action.payload);
     default:
       return state;
   }
