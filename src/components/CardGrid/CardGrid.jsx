6bd1bb613751520d9a2c6a6d551067baff27c2e4
Ahmed Modan <ahmed.modan@gmail.com>
2016-04-13 12:29:48 -0700

    [feat] display pins/cards

--- src/components/CardGrid/CardGrid.jsx
+++ src/components/CardGrid/CardGrid.jsx
@@ -7,7 +7,7 @@ require('./cardgrid.scss');
 export function CardGrid({ arrOfCards }) {
   return (
     <div className="card-columns card-grid">
-      { arrOfCards.map((card) => <Card key={ card } title={ card } />) }
+      { arrOfCards.map((card) => <Card key={ card._id } {...card} />) }
     </div>
   );
 }
