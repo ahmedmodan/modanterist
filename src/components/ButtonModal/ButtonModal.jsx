b8d775b0c345930c0e02b8559ff0a54579bea93d
Ahmed Modan <ahmed.modan@gmail.com>
2016-05-02 11:59:40 -0700

    [style] modal to materilize

--- src/components/ButtonModal/ButtonModal.jsx
+++ src/components/ButtonModal/ButtonModal.jsx
@@ -3,30 +3,17 @@ import React, { PropTypes } from 'react';
 export function ButtonModal({ buttonName, modalTitle, modalBody, buttonAction }) {
   return (
     <div>
-      <button className="btn btn-secondary"
+      <button className="btn-floating btn-large waves-effect waves-light"
         id="pin-button"
         onClick={ buttonAction }
       >
         { buttonName }
       </button>
 
-      <div className="modal fade" id="myModal"
-        tabIndex="-1" role="dialog"
-        aria-labelledby="myModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times; </span>
-              </button>
-              <h4 className="modal-title" id="myModalLabel">{ modalTitle }</h4>
-            </div>
-            <div className="modal-body">
-              { modalBody }
-            </div>
-          </div>
+      <div id="modal1" className="modal">
+        <div className="modal-content">
+          <h4>{ modalTitle }</h4>
+          { modalBody }
         </div>
       </div>
     </div>
@@ -34,10 +21,9 @@ export function ButtonModal({ buttonName, modalTitle, modalBody, buttonAction })
 }
 
 ButtonModal.propTypes = {
-  buttonName: PropTypes.string.isRequired,
+  buttonName: PropTypes.object.isRequired,
   modalTitle: PropTypes.string.isRequired,
   modalBody: PropTypes.element.isRequired,
-  saveButtonName: PropTypes.string.isRequired,
   buttonAction: PropTypes.func.isRequired,
 };
 
