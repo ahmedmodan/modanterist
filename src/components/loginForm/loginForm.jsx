9ed3127495850e0ef5a52f30f62feb5c364195c9
Ahmed Modan <ahmed.modan@gmail.com>
2016-06-08 02:13:28 -0700

    [refactor] landing login form to use component

--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -1,49 +1,37 @@
 import React, { PropTypes } from 'react';
-import { Link } from 'react-router';
+// import { Link } from 'react-router';
 import { reduxForm } from 'redux-form';
+import SmallFormInput from './../smallFormInput/smallFormInput';
+import { actions as authActions } from './../../redux/modules/auth';
+
+const mapStateToProps = ({ auth }) => ({
+  loggedIn: auth.get('loggedIn')
+});
+
 /* eslint-disable react/prefer-stateless-function */
 export class LoginForm extends React.Component {
   static propTypes = {
     fields: PropTypes.object.isRequired,
     handleSubmit: PropTypes.func.isRequired,
+    toggleLogIn: PropTypes.func.isRequired,
+    loggedIn: PropTypes.bool.isRequired,
   }
 
   render() {
     const { fields: { username, password }, handleSubmit } = this.props;
     return (
       <div className="row">
-        <p className="text-info" >Sign Up below to get started</p>
-        <div className="col-sm-4 col-md-4 col-md-offset-2 col-sm-offset-1">
-          <form className="form-horizontal" onSubmit={handleSubmit} >
-            <div className="form-group">
-              <label htmlFor="username-input" >Username</label>
-              <input type="text"
-                className="form-control"
-                placeholder="Username"
-                id="username-input"
-                { ...username }
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password-input">Password</label>
-              <input type="text"
-                className="form-control"
-                placeholder="Password"
-                id="password-input"
-                { ...password }
-              />
-            </div>
-            <div className="form-group">
-              <Link to="counter">
-                <button className="btn btn-primary" type="submit">Sign Up</button>
-              </Link>
-            </div>
-          </form>
-          <p>
-            Already have an account?
-            <Link to="counter/hello"> Login here</Link>
-          </p>
-        </div>
+        <form onSubmit={ handleSubmit(this.props.toggleLogIn) } >
+          <div className="col s6 offset-s3">
+            <SmallFormInput field={ username } inputLabel="Username" />
+          </div>
+          <div className="col s6 offset-s3">
+            <SmallFormInput field={ password } inputLabel="Password" />
+          </div>
+          <div className="col offset-s6 offset-m8">
+            <button type="submit" className="btn waves-effect wave-light">Log In</button>
+          </div>
+        </form>
       </div>
     );
   }
@@ -52,4 +40,4 @@ export class LoginForm extends React.Component {
 export default reduxForm({
   form: 'loginForm',
   fields: ['username', 'password']
-})(LoginForm);
+}, mapStateToProps, authActions)(LoginForm);
